91bef4a8f84b5f2d1dbffc83d03c569174520e4b
Jason Kulatunga <jk17@ualberta.ca>
2013-11-07 20:06:56 -0800

    allow versions to be specified and locked within the recipe.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,71 +4,71 @@ default['scipy']['install_type'] = "package"
 case node['platform_family']
 when "debian"
   if node['platform'] == "ubuntu" && node['platform_version'] == "12.04"
-    default['scipy']['packages'] = %w{
-      gfortran
-      liblapack-dev
-      libopenblas-dev
-      python-dev
-      python-matplotlib
-      python-nose
-      python-numpy
-      python-sympy
-    }
-    default['scipy']['pip_packages'] = %w{
-      ipython
-      scipy
-      pandas
-    }
+    default['scipy']['packages'] = [
+      ["gfortran", nil],
+      ["liblapack-dev", nil],
+      ["libopenblas-dev", nil],
+      ["python-dev", nil],
+      ["python-matplotlib", nil],
+      ["python-nose", nil],
+      ["python-numpy", nil],
+      ["python-sympy", nil],
+    ]
+    default['scipy']['pip_packages'] = [
+      ["ipython", nil],
+      ["scipy", nil],
+      ["pandas", nil],
+    ]
   else
-    default['scipy']['packages'] = %w{
-      ipython
-      ipython-notebook
-      python-numpy
-      python-scipy
-      python-matplotlib
-      python-nose
-      python-pandas
-      python-sympy
-    }
-    default['scipy']['pip_packages'] = %w{}
+    default['scipy']['packages'] = [
+      ["ipython", nil],
+      ["ipython-notebook", nil],
+      ["python-numpy", nil],
+      ["python-scipy", nil],
+      ["python-matplotlib", nil],
+      ["python-nose", nil],
+      ["python-pandas", nil],
+      ["python-sympy", nil]
+    ]
+    default['scipy']['pip_packages'] = []
   end
 when "fedora"
-  default['scipy']['packages'] = %w{
-    ipython
-    numpy
-    scipy
-    python-matplotlib
-    python-nose
-    python-pandas
-    sympy
-  }
-  default['scipy']['pip_packages'] = %w{}
+  default['scipy']['packages'] = [
+    ["ipython", nil],
+    ["numpy", nil],
+    ["scipy", nil],
+    ["python-matplotlib", nil],
+    ["python-nose", nil],
+    ["python-pandas", nil],
+    ["sympy", nil],
+  ]
+  default['scipy']['pip_packages'] = []
 when "gentoo"
-  default['scipy']['packages'] = %w{
-    '>=dev-python/ipython-0.13'
-    '>=dev-python/matplotlib-1.1'
-    '>=dev-python/nose-1.1'
-    '>=dev-python/numpy-1.6'
-    '>=dev-python/pandas-0.8'
-    '>=dev-python/sympy-0.7'
-    '>=sci-libs/scipy-0.10'
-  }
-  default['scipy']['pip_packages'] = %w{}
+  default['scipy']['packages'] = [
+    ['>=dev-python/ipython-0.13', nil],
+    ['>=dev-python/matplotlib-1.1', nil],
+    ['>=dev-python/nose-1.1', nil],
+    ['>=dev-python/numpy-1.6', nil],
+    ['>=dev-python/pandas-0.8', nil],
+    ['>=dev-python/sympy-0.7', nil],
+    ['>=sci-libs/scipy-0.10', nil]
+  ]
+  default['scipy']['pip_packages'] = []
 when "rhel"
-  default['scipy']['packages'] = %w{
-    blas-devel
-    freetype-devel
-    lapack-devel
-    libpng-devel
-    python-devel
-  }
-  default['scipy']['pip_packages'] = %w{
-    ipython
-    numpy
-    scipy
-    matplotlib
-    nose
-    pandas
-    sympy
-  }
+  default['scipy']['packages'] = [
+    ["blas-devel", nil],
+    ["freetype-devel", nil],
+    ["lapack-devel", nil],
+    ["libpng-devel", nil],
+    ["python-devel", nil]
+  ]
+  default['scipy']['pip_packages'] = [
+    ["ipython", nil],
+    ["numpy", nil],
+    ["scipy", nil],
+    ["matplotlib", nil],
+    ["nose", nil],
+    ["pandas", nil],
+    ["sympy", nil]
+  ]
 end
