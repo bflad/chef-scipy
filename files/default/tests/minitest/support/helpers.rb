9e7a78f953c8952c4499cd6a6290ce9f75fdcfb2
Brian Flad <bflad417@gmail.com>
2013-12-08 22:31:16 -0500

    Rubocop fixes

--- files/default/tests/minitest/support/helpers.rb
+++ files/default/tests/minitest/support/helpers.rb
@@ -1,8 +1,9 @@
+# Helpers module
 module Helpers
+  # Helpers::Scipy module
   module Scipy
     include MiniTest::Chef::Assertions
     include MiniTest::Chef::Context
     include MiniTest::Chef::Resources
-
   end
 end
